49e3bec1af758a5e1a1c0332f9ec264285ddb30f
Rebeca Yun <rebecajhyun@gmail.com>
2024-02-26 13:58:36 -0300

    setup

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,6 @@
+// app/assets/config/manifest.js
+//= link popper.js
+//= link bootstrap.min.js
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,3 +1,13 @@
+# config/importmap.rb
+
+# replace these lines:
+# pin "bootstrap" # @5.3.2
+# pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
+
+# with this:
+pin "bootstrap", to: "bootstrap.min.js", preload: true
+pin "@popperjs/core", to: "popper.js", preload: true
+
 # Pin npm packages by running ./bin/importmap
 
 pin "application"
@@ -5,3 +15,5 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "bootstrap" # @5.3.3
+pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
